69d2160333c9c95068519555148fd2a6ff9e687f
sarleone <sarah.m.leone@gmail.com>
2018-03-28 14:36:52 -0500

    tweeks

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -14,12 +14,12 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
 };
